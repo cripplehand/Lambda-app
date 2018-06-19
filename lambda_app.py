3325c5259dcfce4d8a4d8eeb8969df13b606c1b1
cripplehand <1628763frijns@zuyd.nl>
2018-06-19 12:02:25 +0200

    new file

---
+++ lambda_app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Conform PEP8 http://pep8online.com/
+
+# Imports
+
+# Defs
+
+# Inilization
+
+# Program
+
+print("Hoi Camiel. Ik weet het. Waarom Lambda_app? Idk vind grieks alphabet wel leuk. MS klinkt zo gay.")
+
