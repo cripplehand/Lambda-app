3b1dd1a38400a679c676db107626386a411dd360
Macley <wesley.de.vree@gmail.com>
2018-06-19 21:35:09 +0200

    Meer comments

--- lambda_app.py
+++ lambda_app.py
@@ -14,6 +14,22 @@
 ## Indien SLB = toon slb functies
 ## Indien STUDENT = toon student functies
 
+# Toon menu op basis van functies
+
+## Functie 0
+### Afsluiten
+
+## Functie 1
+
+## Functie 2
+
+## Functie 3
+
+## Functie 4
+
+## Functie 5
+
+## Functie 6
 
 
 
