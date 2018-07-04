3467adcf89129eb7dc2fa2e5137c96ae90fc91a6
Macley <wesley.de.vree@gmail.com>
2018-07-04 10:02:38 +0200

    pep8 MAIN FILE

--- lambda_app.py
+++ lambda_app.py
@@ -3,11 +3,11 @@
 # Conform PEP8 http://pep8online.com/
 
 # Imports
-from lambda_app_UI import * # Importeren menu's (UI)
-from lambda_app_RQ import * # Importeren menu's (RQ)
-import os #  Opschoonen van de termianl
+from lambda_app_UI import *  # Importeren menu's (UI)
+from lambda_app_RQ import *  # Importeren menu's (RQ)
+import os  # Opschoonen van de terminal
 
-# Program 
+# Program
 
 # Groet gebruiker tot de applicatie.
 welcome()
@@ -16,44 +16,3 @@ os.system('cls')
 # Login scherm
 login_msg()
 login()
-
-
-
-# Valideren
-## Indien SLB = toon slb functies
-## Indien STUDENT = toon student functies
-
-# Toon menu op basis van functiesf
-
-
-
-
-
-
-
-    
-
-## Functie 0
-### Afsluiten *
-
-## Functie 1
-###Opvragen aanbod onderwijseenheden (1a) *
-## Functie 2
-###Opvragen aanbod onderwijseenheden extern (1a) *
-## Functie 3
-###STUDENT ONLY:
-###        invullen studieboefte op basis van de onderwijseenheden die beschikbaar zijn (kenniselementen) * 
-## Functie 4
-###STUDENT ONLY:
-###    (Indien) studiebehoefte niet volledig is, kan exterene kenniselkementen gezocht worden (minors/courses) (2) * 
-## Functie 5
-###STUDENT ONLY:
-###    het verkrijgen van een overzicht van wat hij/zij wilt volgen in het komend blok. (3) * 
-## Functie 6
-###SLB ONLY!:
-###    goedkeuren/afkeuren voorlopige studiepad (4)
-
-
-
-
-
