cf6b1160fbc5fa2760d8fa118372f4105277045a
Macley <wesley.de.vree@gmail.com>
2018-07-04 10:04:06 +0200

    PEP8 UI FILE

--- lambda_app_UI.py
+++ lambda_app_UI.py
@@ -84,11 +84,11 @@ def mainmenu_student():
 |                   |    | Onderwijseenheden |    | Onderwijseenheden |
 +-------------------+    +-------------------+    +-------------------+
 
-+-------------------+    +-------------------+ 
-|      Keuze 3      |    |      Keuze 4      | 
-|  Toon Studieplan  |    |     Invoeren      | 
-|                   |    |  studiebehoefte   | 
-+-------------------+    +-------------------+ 
++-------------------+    +-------------------+
+|      Keuze 3      |    |      Keuze 4      |
+|  Toon Studieplan  |    |     Invoeren      |
+|                   |    |  studiebehoefte   |
++-------------------+    +-------------------+
 
 
 
@@ -117,11 +117,11 @@ def mainmenu_slb():
 |                   |    | Onderwijseenheden |    | Onderwijseenheden |
 +-------------------+    +-------------------+    +-------------------+
 
-+-------------------+    +-------------------+   
-|      Keuze 3      |    |      Keuze 4      |   
-|       Tonen       |    |     Goedkeuren    |   
-|   Studieplannen   |    |   Studieplannen   |   
-+-------------------+    +-------------------+   
++-------------------+    +-------------------+
+|      Keuze 3      |    |      Keuze 4      |
+|       Tonen       |    |     Goedkeuren    |
+|   Studieplannen   |    |   Studieplannen   |
++-------------------+    +-------------------+
 
 
 
@@ -131,7 +131,7 @@ Kies uit: 0,1,2,3,4 of 5
 
 
 def menu_intern():
-        print("""
+    print("""
 
 
             +----------------------------------------------+
@@ -150,11 +150,11 @@ def menu_intern():
 |      Hoofdmenu    |    |                   |    |                   |
 +-------------------+    +-------------------+    +-------------------+
 
-+-------------------+    +-------------------+   
-|      Keuze 3      |    |      Keuze 4      |   
-|      Business     |    |   IT-Development  |   
-|Intelligence vakken|    |       vakken      |   
-+-------------------+    +-------------------+   
++-------------------+    +-------------------+
+|      Keuze 3      |    |      Keuze 4      |
+|      Business     |    |   IT-Development  |
+|Intelligence vakken|    |       vakken      |
++-------------------+    +-------------------+
 
 
 
@@ -180,11 +180,11 @@ def overzicht_keuze_blok():
 |      Hoofdmenu    |    |   studiebehoefte  |    |   studiebehoefte  |
 +-------------------+    +-------------------+    +-------------------+
 
-+-------------------+    +-------------------+ 
-|      Keuze 3      |    |      Keuze 4      | 
-|  Blok 3 invoeren  |    |  Blok 4 invoeren  | 
-|   studiebehoefte  |    |   studiebehoefte  | 
-+-------------------+    +-------------------+ 
++-------------------+    +-------------------+
+|      Keuze 3      |    |      Keuze 4      |
+|  Blok 3 invoeren  |    |  Blok 4 invoeren  |
+|   studiebehoefte  |    |   studiebehoefte  |
++-------------------+    +-------------------+
 
 
 
